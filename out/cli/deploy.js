00661a20b3e8aa96d021f2b2cb746e1da34f4a38
Christy Haragan <christy.haragan.github@gmail.com>
2020-05-15 19:03:36 +0100

    Initial commit

--- out/cli/deploy.js
+++ out/cli/deploy.js
@@ -1,15 +1,7 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const settings_1 = require("../settings");
 const deployer_1 = require("../deployer");
-const path = __importStar(require("path"));
 function default_1() {
     let settings = settings_1.load_settings_env(process.cwd());
     if (settings) {
@@ -19,14 +11,15 @@ function default_1() {
         }
         else {
             (async function () {
-                if (settings.protocol.src_fn_name) {
-                    await deployer_1.deploy_source(path.join(process.cwd(), 'src'), false, '', settings.protocol.src_fn_name, settings.protocol.work_slug, settings.server.jwt_token);
-                    console.log('Deployed source ' + settings.protocol.src_fn_name);
-                }
-                if (settings.protocol.dest_fn_name) {
-                    await deployer_1.deploy_destination(path.join(process.cwd(), 'src'), false, '', settings.protocol.dest_fn_name, settings.protocol.work_slug, settings.server.jwt_token);
-                    console.log('Deployed destination ' + settings.protocol.dest_fn_name);
-                }
+                await deployer_1.deploy_functions(settings, '');
+                // if (settings.protocol.src_fn_name) {
+                //   await deploy_source(path.join(process.cwd(), 'src'), false, '', settings.protocol.src_fn_name, settings.protocol.work_slug, settings.server.jwt_token)
+                //   console.log('Deployed source ' + settings.protocol.src_fn_name)
+                // }
+                // if (settings.protocol.dest_fn_name) {
+                //   await deploy_destination(path.join(process.cwd(), 'src'), false, '', settings.protocol.dest_fn_name, settings.protocol.work_slug, settings.server.jwt_token)
+                //   console.log('Deployed destination ' + settings.protocol.dest_fn_name)
+                // }
             })();
         }
     }
--- src/cli/deploy.ts
+++ src/cli/deploy.ts
@@ -1,5 +1,5 @@
 import { load_settings_env } from '../settings'
-import { deploy_source, deploy_destination } from '../deployer'
+import { deploy_source, deploy_destination, deploy_functions } from '../deployer'
 import * as path from 'path'
 
 export default function () {
@@ -10,14 +10,15 @@ export default function () {
       console.error(JSON.stringify(settings))
     } else {
       (async function () {
-        if (settings.protocol.src_fn_name) {
-          await deploy_source(path.join(process.cwd(), 'src'), false, '', settings.protocol.src_fn_name, settings.protocol.work_slug, settings.server.jwt_token)
-          console.log('Deployed source ' + settings.protocol.src_fn_name)
-        }
-        if (settings.protocol.dest_fn_name) {
-          await deploy_destination(path.join(process.cwd(), 'src'), false, '', settings.protocol.dest_fn_name, settings.protocol.work_slug, settings.server.jwt_token)
-          console.log('Deployed destination ' + settings.protocol.dest_fn_name)
-        }
+        await deploy_functions(settings, '')
+        // if (settings.protocol.src_fn_name) {
+        //   await deploy_source(path.join(process.cwd(), 'src'), false, '', settings.protocol.src_fn_name, settings.protocol.work_slug, settings.server.jwt_token)
+        //   console.log('Deployed source ' + settings.protocol.src_fn_name)
+        // }
+        // if (settings.protocol.dest_fn_name) {
+        //   await deploy_destination(path.join(process.cwd(), 'src'), false, '', settings.protocol.dest_fn_name, settings.protocol.work_slug, settings.server.jwt_token)
+        //   console.log('Deployed destination ' + settings.protocol.dest_fn_name)
+        // }
       })()
     }
   }
